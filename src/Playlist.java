0e00aefb7b3a7b88dc85fc9bda3b9d24aecba830
brnunez15 <brnunez@uade.edu.ar>
2026-06-08 16:10:44 -0300

    implements SpotifiArbol

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,6 +1,21 @@
 import java.util.List;
 
-public class Playlist implements espotifi{
+public class Playlist implements SpotifiArbol{
     private List<Cancion> canciones;
     private String nombre;
+
+    @Override
+    public void insertarCancion(int id) {
+
+    }
+
+    @Override
+    public void eliminarCancion(int id) {
+
+    }
+
+    @Override
+    public String buscarCancionXNombre(String nombre) {
+        return "";
+    }
 }
